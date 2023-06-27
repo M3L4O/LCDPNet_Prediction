cca613bac2c41c7124bbaf556b770a7afbf9c695
M3L4O <joaovictor.slv@ufpi.edu.br>
2023-06-27 03:16:50 -0300

    add prediction

--- src/data/img_dataset.py
+++ src/data/img_dataset.py
@@ -2,6 +2,7 @@
 import random
 from collections.abc import Iterable
 from glob import glob
+import os.path as ph
 
 import cv2
 import ipdb
@@ -15,16 +16,18 @@ from .augmentation import parseAugmentation
 
 
 def parse_item_list_txt(txtpath):
-    '''
+    """
     Parse txt file containing all file paths. Each line one file.
-    '''
+    """
     txt = Path(txtpath)
     basedir = txt.parent
     content = txt.open().read().splitlines()
 
     sample = content[0]
-    if sample.split('/')[0] in str(basedir):
-        raise NotImplementedError('Not implemented: file path in txt and basedir have comman str.')
+    if sample.split("/")[0] in str(basedir):
+        raise NotImplementedError(
+            "Not implemented: file path in txt and basedir have comman str."
+        )
     else:
         assert (basedir / sample).exists()
         return [str(basedir / x) for x in content]
@@ -32,8 +35,8 @@ def parse_item_list_txt(txtpath):
 
 def load_from_glob_list(globs):
     if type(globs) == str:
-        if globs.endswith('.txt'):
-            print(f'Parse txt file: {globs}')
+        if globs.endswith(".txt"):
+            print(f"Parse txt file: {globs}")
             return parse_item_list_txt(globs)
         else:
             return sorted(glob(globs))
@@ -42,13 +45,14 @@ def load_from_glob_list(globs):
         # if iamges are glob lists, sort EACH list ** individually **
         res = []
         for g in globs:
-            assert not g.endswith('.txt'), 'TXT file should not in glob list.'
+            assert not g.endswith(".txt"), "TXT file should not in glob list."
             res.extend(sorted(glob(g)))
         return res
     else:
         ipdb.set_trace()
         raise TypeError(
-            f'ERR: `ds.GT` or `ds.input` has wrong type: expect `str` or `list` but get {type(globs)}')
+            f"ERR: `ds.GT` or `ds.input` has wrong type: expect `str` or `list` but get {type(globs)}"
+        )
 
 
 def augment_one_img(img, seed, transform=None):
@@ -61,7 +65,6 @@ def augment_one_img(img, seed, transform=None):
 
 
 class ImagesDataset(torch.utils.data.Dataset):
-
     def __init__(self, opt, ds_type=TRAIN_DATA, transform=None, batchsize=None):
         """Initialisation for the Dataset object
         transform: PyTorch image transformations to apply to the images
@@ -73,8 +76,8 @@ class ImagesDataset(torch.utils.data.Dataset):
         input_globs = opt[ds_type][INPUT]
         self.have_gt = True if gt_globs else False
 
-        print(f'{ds_type} - GT Directory path: [yellow]{gt_globs}[/yellow]')
-        print(f'{ds_type} - Input Directory path: [yellow]{input_globs}[/yellow]')
+        print(f"{ds_type} - GT Directory path: [yellow]{gt_globs}[/yellow]")
+        print(f"{ds_type} - Input Directory path: [yellow]{input_globs}[/yellow]")
 
         # load input images:
         self.input_list = load_from_glob_list(input_globs)
@@ -87,24 +90,26 @@ class ImagesDataset(torch.utils.data.Dataset):
             except:
                 ipdb.set_trace()
                 raise AssertionError(
-                    f'In [{ds_type}]: len(input_images) ({len(self.input_list)}) != len(gt_images) ({len(self.gt_list)})! ')
+                    f"In [{ds_type}]: len(input_images) ({len(self.input_list)}) != len(gt_images) ({len(self.gt_list)})! "
+                )
 
         # import ipdb; ipdb.set_trace()
         print(
-            f'{ds_type} Dataset length: {self.__len__()}, batch num: {self.__len__() // batchsize}')
+            f"{ds_type} Dataset length: {self.__len__()}, batch num: {self.__len__() // batchsize}"
+        )
 
         if self.__len__() == 0:
-            print(f'Error occured! Your ds is: TYPE={ds_type}, config:')
+            print(f"Error occured! Your ds is: TYPE={ds_type}, config:")
             print(opt[ds_type])
-            raise RuntimeError(f'[ Err ] Dataset input nums is 0!')
+            raise RuntimeError(f"[ Err ] Dataset input nums is 0!")
 
     def __len__(self):
-        return (len(self.input_list))
+        return len(self.input_list)
 
     def debug_save_item(self, input, gt):
         # home = os.environ['HOME']
-        util.saveTensorAsImg(input, 'i.png')
-        util.saveTensorAsImg(gt, 'o.png')
+        util.saveTensorAsImg(input, "i.png")
+        util.saveTensorAsImg(gt, "o.png")
 
     def __getitem__(self, idx):
         """Returns a pair of images with the given identifier. This is lazy loading
@@ -123,7 +128,7 @@ class ImagesDataset(torch.utils.data.Dataset):
         # random.seed(GLOBAL_SEED)
         seed = random.randint(0, 100000)
         input_img = cv2.imread(self.input_list[idx])[:, :, [2, 1, 0]]
-        if self.have_gt and self.gt_list[idx].endswith('.hdr'):
+        if self.have_gt and self.gt_list[idx].endswith(".hdr"):
             input_img = torch.Tensor(input_img / 255).permute(2, 0, 1)
         else:
             input_img = augment_one_img(input_img, seed, transform=self.transform)
@@ -132,10 +137,12 @@ class ImagesDataset(torch.utils.data.Dataset):
         if self.have_gt:
             res_item[GT_FPATH] = self.gt_list[idx]
 
-            if res_item[GT_FPATH].endswith('.hdr'):
+            if res_item[GT_FPATH].endswith(".hdr"):
                 # gt may be HDR
                 # do not augment HDR image.
-                gt_img = cv2.imread(self.gt_list[idx], flags=cv2.IMREAD_ANYDEPTH)[:, :, [2, 1, 0]]
+                gt_img = cv2.imread(self.gt_list[idx], flags=cv2.IMREAD_ANYDEPTH)[
+                    :, :, [2, 1, 0]
+                ]
                 gt_img = torch.Tensor(np.log10(gt_img + 1)).permute(2, 0, 1)
             else:
                 gt_img = cv2.imread(self.gt_list[idx])[:, :, [2, 1, 0]]
@@ -178,28 +185,46 @@ class DataModule(LightningDataModule):
         if stage == "fit":
             # if opt[TRAIN_DATA]:
             assert opt[TRAIN_DATA][INPUT]
-            self.training_dataset = ImagesDataset(opt, ds_type=TRAIN_DATA, transform=self.transform, batchsize=opt.batchsize)
+            self.training_dataset = ImagesDataset(
+                opt,
+                ds_type=TRAIN_DATA,
+                transform=self.transform,
+                batchsize=opt.batchsize,
+            )
 
             # valid data provided:
             if opt[VALID_DATA] and opt[VALID_DATA][INPUT]:
-                self.valid_dataset = ImagesDataset(opt, ds_type=VALID_DATA, transform=self.valid_transform, batchsize=opt.valid_batchsize)
+                self.valid_dataset = ImagesDataset(
+                    opt,
+                    ds_type=VALID_DATA,
+                    transform=self.valid_transform,
+                    batchsize=opt.valid_batchsize,
+                )
 
             # no valid data, splt from training data:
             elif opt[VALID_RATIO]:
-                print(f'Split valid dataset from training data. Ratio: {opt[VALID_RATIO]}')
+                print(
+                    f"Split valid dataset from training data. Ratio: {opt[VALID_RATIO]}"
+                )
                 valid_size = int(opt[VALID_RATIO] * len(self.training_dataset))
                 train_size = len(self.training_dataset) - valid_size
                 torch.manual_seed(233)
-                self.training_dataset, self.valid_dataset = torch.utils.data.random_split(self.training_dataset, [
-                    train_size, valid_size
-                ])
+                (
+                    self.training_dataset,
+                    self.valid_dataset,
+                ) = torch.utils.data.random_split(
+                    self.training_dataset, [train_size, valid_size]
+                )
                 print(
-                    f'Update - training data: {len(self.training_dataset)}; valid data: {len(self.valid_dataset)}')
+                    f"Update - training data: {len(self.training_dataset)}; valid data: {len(self.valid_dataset)}"
+                )
 
         # testing phase
         # if stage == 'test':
         if opt[TEST_DATA] and opt[TEST_DATA][INPUT]:
-            self.test_dataset = ImagesDataset(opt, ds_type=TEST_DATA, transform=self.test_transform, batchsize=1)
+            self.test_dataset = ImagesDataset(
+                opt, ds_type=TEST_DATA, transform=self.test_transform, batchsize=1
+            )
 
     def train_dataloader(self):
         if self.training_dataset:
@@ -209,7 +234,7 @@ class DataModule(LightningDataModule):
                 num_workers=self.opt[DATALOADER_N],
                 shuffle=True,
                 drop_last=True,
-                pin_memory=True
+                pin_memory=True,
             )
             return trainloader
 
@@ -219,7 +244,7 @@ class DataModule(LightningDataModule):
                 self.valid_dataset,
                 batch_size=self.opt[VALID_BATCHSIZE],
                 shuffle=False,
-                num_workers=self.opt[DATALOADER_N]
+                num_workers=self.opt[DATALOADER_N],
             )
 
     def test_dataloader(self):
@@ -229,7 +254,7 @@ class DataModule(LightningDataModule):
                 batch_size=1,
                 shuffle=False,
                 num_workers=self.opt[DATALOADER_N],
-                pin_memory=True
+                pin_memory=True,
             )
 
     def teardown(self, stage):
@@ -238,5 +263,28 @@ class DataModule(LightningDataModule):
         ...
 
 
-if __name__ == '__main__':
+class ColorCorrectionDataset(Dataset):
+    def __init__(
+        self,
+        image_dir: str,
+        image_size: int,
+    ) -> None:
+        self.images_id = glob(ph.join(image_dir, "*"))
+        self.image_size = image_size
+
+    def __len__(self) -> int:
+        return len(self.images_id)
+
+    def __getitem__(self, index: int) -> tuple:
+        image_id = self.images_id[index]
+        image_shape = (self.image_size, self.image_size)
+        image = cv2.resize(cv2.imread(image_id, cv2.IMREAD_COLOR), image_shape)
+        image = image.astype(np.float64)
+        image = np.moveaxis(image, -1, 0)
+        image = image / 255.0
+
+        return image, image_id
+
+
+if __name__ == "__main__":
     ...
